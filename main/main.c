e5c5c7e0fd0a0064ebcebc9066ebe9413aacee8a
shadow <MrHload163@163.com>
2023-12-21 15:36:51 +0800

    0.1.1_freertos多任务

--- main/main.c
+++ main/main.c
@@ -2,7 +2,7 @@
  * @Author: shadow MrHload163@163.com
  * @Date: 2023-12-19 15:22:02
  * @LastEditors: shadow MrHload163@163.com
- * @LastEditTime: 2023-12-21 11:16:07
+ * @LastEditTime: 2023-12-21 15:34:18
  * @FilePath: \SmartLock\main\main.c
  * @Description:
  */
@@ -12,22 +12,66 @@
 #include "freertos/task.h"
 #include "MG90S.h"
 
-#define VERSION "0.1.0"
+#define VERSION "0.1.1"
+
+static TaskHandle_t LED_Task_Handle = NULL;   /* LED 任务句柄 */
+static TaskHandle_t MG90S_Task_Handle = NULL; /* MG90S 任务句柄 */
+
+static void LED_Task(void *pvParameters);   /* LED_Task 任务实现 */
+static void MG90S_Task(void *pvParameters); /* MG90S_Task 任务实现 */
 
 void app_main(void)
 {
+    BaseType_t xReturn = pdPASS; /* 定义一个创建信息返回值，默认为 pdPASS */
+    led_init();
+    MG90S_Init();
+
     printf("\r\n================================\r\n");
     printf("\r\n-----SMARTLOCK START %s-----\r\n", VERSION);
     printf("\r\n================================\r\n");
 
-    led_init();
-    MG90S_Init();
+    /* 创建 LED_Task 任务 */
+    xReturn = xTaskCreate((TaskFunction_t)LED_Task,          /* 任务入口函数 */
+                          (const char *)"LED_Task",          /* 任务名字 */
+                          (uint16_t)512,                     /* 任务栈大小 */
+                          (void *)NULL,                      /* 任务入口函数参数 */
+                          (UBaseType_t)2,                    /* 任务的优先级 */
+                          (TaskHandle_t *)&LED_Task_Handle); /* 任务控制块指针 */
+    if (pdPASS == xReturn)
+        printf("创建 LED_Task 任务成功!\r\n");
+    else
+        return;
+    /* 创建 MG90S_Task 任务 */
+    xReturn = xTaskCreate((TaskFunction_t)MG90S_Task,          /* 任务入口函数 */
+                          (const char *)"MG90S_Task",          /* 任务名字 */
+                          (uint16_t)512,                       /* 任务栈大小 */
+                          (void *)NULL,                        /* 任务入口函数参数 */
+                          (UBaseType_t)3,                      /* 任务的优先级 */
+                          (TaskHandle_t *)&MG90S_Task_Handle); /* 任务控制块指针 */
+    if (pdPASS == xReturn)
+        printf("创建 MG90S_Task 任务成功!\r\n");
+    else
+        return;
+
+    /* 启动任务调度 */
+    // vTaskStartScheduler();
+}
+
+void LED_Task(void *pvParameters)
+{
     while (1)
     {
-        printf("hello world\r\n");
+        printf("LED TASK\r\n");
         LED1_TOGGLE();
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+void MG90S_Task(void *pvParameters)
+{
+    while (1)
+    {
+        printf("MG90S TASK\r\n");
         MG90S_360_Forward();
-        // vTaskDelay(1000 / portTICK_RATE_MS);
-        // vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
