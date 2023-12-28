0853bc860c256148a02329a5bd6f522b4978354d
shadow <MrHload163@163.com>
2023-12-28 17:48:12 +0800

    V0.2.2_增加mqtt驱动

---
+++ components/mqtt_bemfa/include/mqtt_bemfa.h
@@ -0,0 +1,14 @@
+/*
+ * @Author: shadow MrHload163@163.com
+ * @Date: 2023-12-28 16:53:12
+ * @LastEditors: shadow MrHload163@163.com
+ * @LastEditTime: 2023-12-28 16:54:53
+ * @FilePath: \SmartLock\components\mqtt_bemfa\include\mqtt_bemfa.h
+ * @Description:
+ */
+#ifndef __MQTT_BEMFA_H__
+#define __MQTT_BEMFA_H__
+
+void mqtt_Init(void);
+
+#endif
